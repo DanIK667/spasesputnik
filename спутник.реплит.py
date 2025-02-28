eaff9b5eddeeacbaec290f6a55b8f205095be30e
DanIK667 <122594153+DanIK667@users.noreply.github.com>
2025-02-28 10:13:07 +0300

    Add files via upload

---
+++ спутник.реплит.py
@@ -0,0 +1,39 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Функция, которая строит график выхода на орбиту
+def orbit_entry(altitude):
+  x = np.linspace(0, altitude, 100)
+  y = np.sqrt(x * altitude)
+  plt.plot(x, y, label='Выход на орбиту Земли(высота в км)')
+
+# Функция, которая рисует орбиту планеты
+def draw_orbit(altitude, orbit_speed):
+  t = np.linspace(0, 2 * np.pi, 100)
+  x = altitude * np.cos(t)
+  y = altitude * np.sin(t)
+  plt.plot(x, y, label='Орбита Земли(км)')
+
+# Главная функция, которая будет запускать все остальные и рисовать график
+def main():
+  print("Расчет траектории спутника")
+  print("==========================================")
+  # Начинай писать код на строке снизу
+
+  altitude = float(input("Введите высоту в км: "))
+  orbit_speed = float(input("Введите скорость в км/ч: "))
+
+  orbit_entry(altitude)
+  draw_orbit(altitude, orbit_speed)
+
+  plt.xlabel("Ось X")
+  plt.ylabel("Ось Y")
+  plt.title("Траектория спутника")
+  plt.legend()
+  plt.grid()
+  plt.axis('equal')
+
+  plt.savefig('trajectory.png', dpi=600)
+
+if __name__ == "__main__":
+  main()
